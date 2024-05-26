e9d65581600f3f8d5638156454a46e0aa4c0cfba
aolson28 <123501344+aolson28@users.noreply.github.com>
2024-05-26 13:25:14 -0400

    Update baseThirtySix.js
    add RGB to hex converter

--- stringManipulation/baseThirtySix.js
+++ stringManipulation/baseThirtySix.js
@@ -5,3 +5,18 @@ const incrementBaseThirtySix = (numString) => {
     return JSON.stringify({error: "Not a valid base 36 number"});
   }
 };
+
+const convertRGBToHex = (r, g, b) => {
+    return [r,g,b].map((num) => {
+      if (num <= 0) {
+        return '00'
+      } else if (num < 16) {
+        return '0' + num.toString(16);
+      } else if (num > 255) {
+        return 'FF'
+      } else {
+        return num.toString(16);
+      }
+    }
+  ).join('').toUpperCase();
+};
